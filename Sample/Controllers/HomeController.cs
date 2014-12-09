5e7cee336831e7189222e0a39a72ed515a38286a
uliian <yncxcxz@126.com>
2014-12-09 21:49:41 +0800

    添加示例
    添加示例

---
+++ Sample/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PermissionModule;
+using Sample.Models;
+
+namespace Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string userName, string password)
+        {
+            if (userName.ToUpperInvariant() == "ULIIAN" && password.ToUpperInvariant() == "123456")
+            {
+                Session[SessionName.PermissionSessionName] = new List<PermissionModel>()
+                {
+                    new PermissionModel() {ResourceName = "aaa"},
+                    new PermissionModel() {ResourceName = "ccc"}
+                };
+                return RedirectToAction("Test1");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ResourceCustomerName("aaa")]
+        public ActionResult Test1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ResourceCustomerName("bbb")]
+        public ActionResult Test1(int id)
+        {
+            return View();
+        }
+
+        [ResourceCustomerName("ccc")]
+        public ActionResult Test2()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult NoPermission()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sample/Models/PermissionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PermissionModule;
+
+namespace Sample.Models
+{
+    public class PermissionModel:IAcl
+    {
+        public string ResourceName { get; set; }
+    }
+}
